b7956b2cc754b8d6be86b37cc93d429310dd0d73
Travis CI <yadusurya666@gmail.com>
2025-08-17 16:24:44 +0530

    Sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,42 @@
+/** @type {import('next-sitemap').IConfig} */
+const chapters = [
+    { number: 1, name: "Real Numbers" },
+    { number: 2, name: "Polynomials" },
+    { number: 3, name: "Pair of Linear Equations in Two Variables" },
+    { number: 4, name: "Quadratic Equations" },
+    { number: 5, name: "Arithmetic Progressions" },
+    { number: 6, name: "Triangles" },
+    { number: 7, name: "Coordinate Geometry" },
+    { number: 8, name: "Introduction to Trigonometry" },
+    { number: 9, name: "Some Applications of Trigonometry" },
+    { number: 10, name: "Circles" },
+    { number: 11, name: "Constructions" },
+    { number: 12, name: "Areas Related to Circles" },
+    { number: 13, name: "Surface Areas and Volumes" },
+    { number: 14, name: "Statistics" },
+    { number: 15, name: "Probability" },
+    { number: 'rd',name:"RD Sharma Solutions"}
+  ];
+module.exports = {
+    siteUrl: 'https://learncbse.vercel.app', // your live site URL
+    generateRobotsTxt: true, // (optional) generate robots.txt
+    changefreq: 'weekly',
+    priority: 0.8,
+    sitemapSize: 5000,
+    exclude: ['/admin/*'], // exclude any admin or hidden pages
+    transform: async (config, path) => {
+      return {
+        loc: path, // => this will be exported as https://learncbse.vercel.app/path
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: new Date().toISOString(),
+      };
+    },
+    additionalPaths: async (config) => {
+        return chapters.map(chapter => {
+          const path = `/chapter-${chapter.number}-${chapter.name.toLowerCase().replace(/\s+/g, "-")}`;
+          return config.transform(config, path);
+        });
+      },
+  };
+  
